1bbaea62c7495cac2dd54d7e23e8a35190c2ef29
bgomiss <aduskun@gmail.com>
2023-02-27 22:53:50 +0300

    first commit

--- Speed Tracker/ViewController.swift
+++ Speed Tracker/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    var speedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        requestLocationPermission()
+        speedLabel = UILabel()
+        speedLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(speedLabel)
+        NSLayoutConstraint.activate([
+            speedLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            speedLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])    }
+    func requestLocationPermission() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
-
+   
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            let speed = location.speed
+            speedLabel.text = String(format: "%.2f km/h", speed * 3.6)
+        }
+    }
 }
 
