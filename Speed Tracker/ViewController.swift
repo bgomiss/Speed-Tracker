bc3a66bb6fbd4c85b1c6bf9821b9301b61765fb2
bgomiss <aduskun@gmail.com>
2023-02-27 22:15:27 +0300

    Initial Commit

---
+++ Speed Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Speed Tracker
+//
+//  Created by aycan duskun on 27.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
